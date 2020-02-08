1ec187ba4b6cee54812b503f0b9d23b46facebee
John Abreu <abreuja@bc.edu>
2020-02-08 06:08:22 -0500

    created sell route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,28 @@
 import "./App.css";
 
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
 import ListingList from "./components/ListingList";
 import NavigationBar from "./components/NavigationBar";
 import ProduceFilter from "./components/ProduceFilter";
 import React from "react";
 import data from "./dummy_data.json";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from "react-router-dom"
 
 const App = () => {
-  return ( <
-    div className = "App" >
-    <
-    Router >
-    <
-    NavigationBar / >
-    <
-    Switch >
-    <
-    Route path = "/"
-    component = {
-      Home
-    }
-    />> <
-    /Switch> <
-    /Router> <
-    /div>
+  return (
+    <div className="App">
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route path="/">
+            <ProduceFilter produceCategories={data.produce_categories} />
+            <ListingList listings={data.listings} />
+          </Route>
+          <Route path="/sell"></Route>
+        </Switch>
+      </Router>
+    </div>
   );
 };
-
-const Home = () => {
-  return ( <
-    >
-    <
-    ProduceFilter produceCategories = {
-      data.produce_categories
-    }
-    /> <
-    ListingList listings = {
-      data.listings
-    }
-    /> <
-    />
-  )
-}
 
 export default App;
