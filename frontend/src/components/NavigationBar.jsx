6f65094163040b8415473529810b19cd0cd96c72
John Abreu <abreuja@bc.edu>
2020-02-08 05:15:47 -0500

    MORE CSS YES

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class NavigationBar extends Component {
   render() {
     return (
-      <div className="nav-bar">
+      <div className="nav-bar container">
         <h1>farmy</h1>
       </div>
     );
--- frontend/src/components/ProduceFilter.jsx
+++ frontend/src/components/ProduceFilter.jsx
@@ -6,11 +6,13 @@ export default class ProduceFilter extends Component {
   produceCategories = this.props.produceCategories;
   render() {
     return (
-      <div className="produce-filter container">
-        {this.produceCategories.map(function(produce, i) {
-          console.log(produce);
-          return <ProduceCategory {...produce} key={i} />;
-        })}
+      <div className="produce-filter-container container">
+        <div className="produce-filter">
+          {this.produceCategories.map(function(produce, i) {
+            console.log(produce);
+            return <ProduceCategory {...produce} key={i} />;
+          })}
+        </div>
       </div>
     );
   }
