fdf811e5cc3c936aac206b7154a49d8ee5dda484
Jeong Yeon Cho <chodr@bc.edu>
2020-02-08 13:04:15 -0500

    drop add

--- backend/migrations/20200208100030_set-foreign-keys-to-cascade.js
+++ backend/migrations/20200208100030_set-foreign-keys-to-cascade.js
@@ -1,5 +1,7 @@
 exports.up = async function(knex) {
   await knex.schema.alterTable("User", table => {
+    table.dropColumn("address_id");
+    table.integer("address_id").nullable();
     table
       .foreign("address_id")
       .references("Address.address_id")
@@ -7,6 +9,8 @@ exports.up = async function(knex) {
       .onDelete("CASCADE");
   });
   await knex.schema.alterTable("Listing", table => {
+    table.dropColumn("lister");
+    table.integer("lister");
     table
       .foreign("lister")
       .references("User.user_id")
@@ -16,6 +20,8 @@ exports.up = async function(knex) {
   });
 
   await knex.schema.alterTable("Request", table => {
+    table.drop("requester");
+    table.integer("requester");
     table
       .foreign("requester")
       .references("User.user_id")
