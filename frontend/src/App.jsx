e4803f654bc2547fb84686abea4a88b2e3862cb3
Jeong Yeon Cho <chodr@bc.edu>
2020-02-08 05:29:13 -0500

    incorporated react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,47 @@ import NavigationBar from "./components/NavigationBar";
 import ProduceFilter from "./components/ProduceFilter";
 import React from "react";
 import data from "./dummy_data.json";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom"
 
 const App = () => {
-  return (
-    <div className="App">
-      <NavigationBar />
-      <ProduceFilter produceCategories={data.produce_categories} />
-      <ListingList listings={data.listings} />
-    </div>
+  return ( <
+    div className = "App" >
+    <
+    Router >
+    <
+    NavigationBar / >
+    <
+    Switch >
+    <
+    Route path = "/"
+    component = {
+      Home
+    }
+    />> <
+    /Switch> <
+    /Router> <
+    /div>
   );
 };
+
+const Home = () => {
+  return ( <
+    >
+    <
+    ProduceFilter produceCategories = {
+      data.produce_categories
+    }
+    /> <
+    ListingList listings = {
+      data.listings
+    }
+    /> <
+    />
+  )
+}
 
 export default App;
